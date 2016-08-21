39a8595ef0db56929f225466d8ccf8eee971ec7b
Sam Serrels <doogly1@gmail.com>
2016-08-21 18:15:05 +0100

    Initial Commit

---
+++ cmake_build_vs15.bat
@@ -0,0 +1,17 @@
+echo off
+where /q cmake
+IF %ERRORLEVEL%==1 (
+    Echo cmake not in path
+	SET p86=C:\Program Files (x86)
+	PATH=%path%;%ProgramFiles(x86)%\CMake\bin
+	PATH=%path%;%ProgramFiles%\\CMake\bin
+ )
+where /q cmake
+IF %ERRORLEVEL%==1 (
+    Echo Couldn't find cmake.exe
+) ELSE ( 
+   echo Cmake building for VS2015 Win64
+   cmake -G "Visual Studio 14 2015 Win64"
+   echo cmake complete, open the sln in the build dir
+ )
+pause > nul
