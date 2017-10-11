f93ff64166786d59f54ffaf63204caaa85451753
Sam Serrels <doogly1@gmail.com>
2017-10-11 14:42:18 +0100

    Staged Repos

--- test/test.cpp
+++ test/test.cpp
@@ -23,10 +23,11 @@ bool load_content() {
   // Create triangle data
   // Positions
   geom.set_type(GL_TRIANGLE_STRIP);
-  vector<vec3> positions{vec3(1.0f, 0.0f, 1.0f), vec3(1.0f, 0.0f, -1.0f), vec3(-1.0f, 0.0f, 1.0f),
-                         vec3(-1.0f, 0.0f, -1.0f)};
+  vector<vec3> positions{vec3(1.0f, 0.0f, 1.0f), vec3(1.0f, 0.0f, -1.0f),
+                         vec3(-1.0f, 0.0f, 1.0f), vec3(-1.0f, 0.0f, -1.0f)};
   // Texture coordinates
-  vector<vec2> tex_coords{vec2(0.0f, 0.0f), vec2(40.0f, 0.0f), vec2(0.0f, 40.0f), vec2(40.0f, 40.0f)};
+  vector<vec2> tex_coords{vec2(0.0f, 0.0f), vec2(40.0f, 0.0f),
+                          vec2(0.0f, 40.0f), vec2(40.0f, 40.0f)};
   // Add to the geometry
   geom.add_buffer(positions, BUFFER_INDEXES::POSITION_BUFFER);
 
@@ -44,12 +45,15 @@ bool load_content() {
   src = "textures/sahara_lf.jpg";
   tjpg = texture(src, false, false);
 
-  array<string, 6> filenames = {"textures/sahara_ft.jpg", "textures/sahara_bk.jpg", "textures/sahara_up.jpg",
-                                "textures/sahara_dn.jpg", "textures/sahara_rt.jpg", "textures/sahara_lf.jpg"};
+  array<string, 6> filenames = {
+      "textures/sahara_ft.jpg", "textures/sahara_bk.jpg",
+      "textures/sahara_up.jpg", "textures/sahara_dn.jpg",
+      "textures/sahara_rt.jpg", "textures/sahara_lf.jpg"};
   cube_map = cubemap(filenames);
 
-  vector<string> mipnames = {"textures/uv_32.png", "textures/uv_16.png", "textures/uv_8.png",
-                             "textures/uv_4.png",  "textures/uv_2.png",  "textures/uv_1.png"};
+  vector<string> mipnames = {"textures/uv_32.png", "textures/uv_16.png",
+                             "textures/uv_8.png",  "textures/uv_4.png",
+                             "textures/uv_2.png",  "textures/uv_1.png"};
 
   tmipped = texture(mipnames, false);
 
@@ -66,7 +70,8 @@ bool load_content() {
   // Set camera properties
   cam.set_position(vec3(10.0f, 10.0f, 10.0f));
   cam.set_target(vec3(0.0f, 0.0f, 0.0f));
-  auto aspect = static_cast<float>(renderer::get_screen_width()) / static_cast<float>(renderer::get_screen_height());
+  auto aspect = static_cast<float>(renderer::get_screen_width()) /
+                static_cast<float>(renderer::get_screen_height());
   cam.set_projection(1.0472f, aspect, 2.414f, 1000.0f);
   return true;
 }
@@ -75,29 +80,34 @@ bool update(float delta_time) {
   // Update the angle - half rotation per second
   theta += pi<float>() * delta_time;
   // Update the camera
-  cam.set_position(rotate(vec3(15.0f, 12.0f, 15.0f), theta * 0.05f, vec3(0, 1.0f, 0)));
+  cam.set_position(
+      rotate(vec3(15.0f, 12.0f, 15.0f), theta * 0.05f, vec3(0, 1.0f, 0)));
   cam.update(delta_time);
 
   if (glfwGetKey(renderer::get_window(), GLFW_KEY_F)) {
     renderer::set_screen_dimensions(1280, 720);
-    auto aspect = static_cast<float>(renderer::get_screen_width()) / static_cast<float>(renderer::get_screen_height());
+    auto aspect = static_cast<float>(renderer::get_screen_width()) /
+                  static_cast<float>(renderer::get_screen_height());
     cam.set_projection(1.0472f, aspect, 2.414f, 1000.0f);
   }
 
   if (glfwGetKey(renderer::get_window(), GLFW_KEY_G)) {
     renderer::set_screen_dimensions(800, 600);
-    auto aspect = static_cast<float>(renderer::get_screen_width()) / static_cast<float>(renderer::get_screen_height());
+    auto aspect = static_cast<float>(renderer::get_screen_width()) /
+                  static_cast<float>(renderer::get_screen_height());
     cam.set_projection(quarter_pi<float>(), aspect, 2.414f, 1000.0f);
   }
 
   if (glfwGetKey(renderer::get_window(), GLFW_KEY_H)) {
     renderer::set_screen_mode(renderer::borderless);
-    auto aspect = static_cast<float>(renderer::get_screen_width()) / static_cast<float>(renderer::get_screen_height());
+    auto aspect = static_cast<float>(renderer::get_screen_width()) /
+                  static_cast<float>(renderer::get_screen_height());
     cam.set_projection(1.0472f, aspect, 2.414f, 1000.0f);
   }
   if (glfwGetKey(renderer::get_window(), GLFW_KEY_J)) {
     renderer::set_screen_mode(renderer::fullscreen);
-    auto aspect = static_cast<float>(renderer::get_screen_width()) / static_cast<float>(renderer::get_screen_height());
+    auto aspect = static_cast<float>(renderer::get_screen_width()) /
+                  static_cast<float>(renderer::get_screen_height());
     cam.set_projection(1.0472f, aspect, 2.414f, 1000.0f);
   }
 
@@ -114,45 +124,47 @@ bool render() {
   auto MVP = P * V * M;
   // Set MVP matrix uniform
   glUniformMatrix4fv(eff.get_uniform_location("MVP"), // Location of uniform
-                     1,                               // Number of values - 1 mat4
-                     GL_FALSE,                        // Transpose the matrix?
-                     value_ptr(MVP));                 // Pointer to matrix data
-                                                      // Render geometry
+                     1,               // Number of values - 1 mat4
+                     GL_FALSE,        // Transpose the matrix?
+                     value_ptr(MVP)); // Pointer to matrix data
+                                      // Render geometry
   renderer::render(geom3);
-  //
 
   renderer::bind(teff);
   renderer::bind(tjpg, 0);
   renderer::bind(tpng, 1);
   renderer::bind(tmipped, 2);
   glUniform1i(teff.get_uniform_location("tex"), 0);
-  glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE,
-                     value_ptr(P * V * translate(mat4(), vec3(3.0f, -5.0f, -8.0f))));
+  glUniformMatrix4fv(
+      teff.get_uniform_location("MVP"), 1, GL_FALSE,
+      value_ptr(P * V * translate(mat4(1.0), vec3(3.0f, -5.0f, -8.0f))));
   renderer::render(geom2);
   glUniform1i(teff.get_uniform_location("tex"), 1);
-  glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE,
-                     value_ptr(P * V * translate(mat4(), vec3(-8.0f, -5.0f, -8.0f))));
+  glUniformMatrix4fv(
+      teff.get_uniform_location("MVP"), 1, GL_FALSE,
+      value_ptr(P * V * translate(mat4(1.0), vec3(-8.0f, -5.0f, -8.0f))));
   renderer::render(geom2);
 
   glUniform1i(teff.get_uniform_location("tex"), 2);
-  glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE,
-                     value_ptr(P * V * translate(mat4(), vec3(0, -10.0f, 0)) * scale(mat4(), vec3(20.0f))));
+  glUniformMatrix4fv(teff.get_uniform_location("MVP"), 1, GL_FALSE,
+                     value_ptr(P * V *
+                               translate(mat4(1.0), vec3(0, -10.0f, 0)) *
+                               scale(mat4(1.0), vec3(20.0f))));
   renderer::render(geom);
 
-   glDisable(GL_DEPTH_TEST);
-  // glDepthMask(GL_FALSE);
+  glDepthMask(GL_FALSE);
   glDisable(GL_CULL_FACE);
 
   renderer::bind(sbeff);
-  MVP = P * V * scale(mat4(), vec3(100.0f));
-  glUniformMatrix4fv(sbeff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
+  MVP = P * V * scale(mat4(1.0), vec3(100.0f));
+  glUniformMatrix4fv(sbeff.get_uniform_location("MVP"), 1, GL_FALSE,
+                     value_ptr(MVP));
   renderer::bind(cube_map, 0);
   glUniform1i(sbeff.get_uniform_location("cubemap"), 0);
   renderer::render(geom4);
 
-  glEnable(GL_CULL_FACE);
-  // glEnable(GL_DEPTH_TEST);
-  // glDepthMask(GL_TRUE);
+  glEnable(GL_DEPTH_TEST);
+  glDepthMask(GL_TRUE);
   return true;
 }
 
